c98d6fc4367b78a947cd2cd0f7492eb7ab8e59ec
AxelHenir <axelhenirmail@gmail.com>
2022-01-17 14:21:24 -0500

    Added / Edited comments

--- Assignments/Sosig Doge/SausageDog.js
+++ Assignments/Sosig Doge/SausageDog.js
@@ -1,33 +1,54 @@
-// SausageDog
-// An extension of the Animal class
-// Adds the idea of being found when clicked
-// and spinning when found
+/*
+Class Sausage Dog
+  - Sausage Dog objects have a position (x,y), an image, and an angle
+  - Sausage Dog objects have a method which updates them (redraw, reposition, etc.)
+  - Sausage Dog is like the "goal" and therefore has extra methods to handle this.
+  - Sausage Dog can be "found" if it's clicked on. It will spin when this happens.
+*/
 
 class SausageDog extends Animal {
-    // constructor(x,y,image)
-    // Calls the super constructor
-    // Adds properties for being found and for a rotation speed
-    constructor(x, y, image) {
-      super(x, y, image);
+    
+  // Constructor
+  constructor(x, y, image) {
+
+    // Use the animal constructor to make the base of the object
+    super(x, y, image);
   
-      this.found = false;
-      this.rotationSpeed = 0.25;
-    }
-  
-    // update()
-    // Calls the super update() and changes angle if found (to rotate!)
-    update() {
-      super.update();
-      if (this.found) {
-        this.angle += this.rotationSpeed;
-      }
-    }
-  
-    // mousePressed()
-    // Checks if this sausage dog was clicked and remembers it was found if so
-    mousePressed() {
-      if (!this.found && this.overlap(mouseX, mouseY)) {
-        this.found = true;
-      }
-    }
+    // New property: found = SD is found or not
+    this.found = false;
+
+    // New property: rotation speed = how fast image rotates when found
+    this.rotationSpeed = 0.25;
+
   }
+  
+  // Updates the SD
+  update() {
+
+      // Use the animal update method
+      super.update();
+
+      // Additionally, check if found
+      if (this.found) {
+
+        // Spin if found
+        this.angle += this.rotationSpeed;
+
+      }
+
+  }
+  
+  // Checks if this sausage dog was clicked and handles it if it's found
+  mousePressed() {
+
+    // Use overlap method from animal class
+    if (!this.found && this.overlap(mouseX, mouseY)) {
+
+      // Set to found if clicked
+      this.found = true;
+
+    }
+
+  }
+
+}
--- Assignments/Sosig Doge/sketch.js
+++ Assignments/Sosig Doge/sketch.js
@@ -1,107 +1,138 @@
+/*
+
+*/
+
 "use strict";
 
-// Constants for image loading
-const NUM_ANIMAL_IMAGES = 10;
-const ANIMAL_IMAGE_PREFIX = `Assets/Images/animal`;
-const SAUSAGE_DOG_IMAGE = `Assets/Images/sausage-dog.png`;
+const NUM_ANIMAL_IMAGES = 10; // Number of unique images
+const ANIMAL_IMAGE_PREFIX = `Assets/Images/animal`; // File location as string (to load easier)
+const SAUSAGE_DOG_IMAGE = `Assets/Images/sausage-dog.png`; // File location of sausage dog
 
 // Number of images to display
 const NUM_ANIMALS = 100;
 
 // Array of the loaded animal images
 let animalImages = [];
+
 // Array of animal objects
 let animals = [];
+
 // Loaded sausage dog image
 let sausageDogImage;
+
 // Sausage dog object
 let sausageDog;
 
-// preload()
-// Loads all the animal images and the sausage dog image
+// Load all the animal images and the sausage dog image
 function preload() {
+
   // Loop once for each animal image, starting from 0
   for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+
     // Load the image with the current number (starting from 0)
     let animalImage = loadImage(`${ANIMAL_IMAGE_PREFIX}${i}.png`);
+
     // Add the image to the array for use later when randomly selecting
     animalImages.push(animalImage);
+
   }
 
   // Load the sausage dog image
   sausageDogImage = loadImage(`${SAUSAGE_DOG_IMAGE}`);
+
 }
 
-
-// setup()
-// Creates all the animal objects and a sausage dog object
+// Create all the animal objects and a sausage dog object
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+  createCanvas(1000, 1000);
 
   createAnimals();
   createSausageDog();
+
 }
 
-// createAnimals()
 // Creates all the animals at random positions with random animal images
 function createAnimals() {
+
   // Create the correct number of animals
   for (let i = 0; i < NUM_ANIMALS; i++) {
+
     // Create one random animal
     let animal = createRandomAnimal();
+
     // Add it to the animals array
     animals.push(animal);
+
   }
+
 }
 
-// createRandomAnimal()
-// Create an animal object at a random position with a random image
-// then return that created animal
+// Creates an animal object at a random position with a random image
 function createRandomAnimal() {
+
+  // Select a random position for the animal
   let x = random(0, width);
   let y = random(0, height);
+
+  // Select a random image for the animal
   let animalImage = random(animalImages);
+
+  // Make a new Animal object
   let animal = new Animal(x, y, animalImage);
+
+  // Return the animal object
   return animal;
+
 }
 
-// createSausageDog()
 // Creates a sausage dog at a random position
 function createSausageDog() {
+
+  // Select a random position for the sausage dog
   let x = random(0, width);
   let y = random(0, height);
+
+  // Generate new SD object and assign it to the global variable
   sausageDog = new SausageDog(x, y, sausageDogImage);
 }
 
-// draw()
-// Draws the background then updates all animals and the sausage dog
+// Draws the background then updates all animals and the sausage dog (called 60 times a second)
 function draw() {
+
+  // White background
   background(255);
 
+  // Update the animals
   updateAnimals();
+
+  // Update the SD
   updateSausageDog();
 }
 
-// updateAnimals()
 // Calls the update() method for all animals
 function updateAnimals() {
+
   // Loop through all animals
   for (let i = 0; i < animals.length; i++) {
+
     // Update the current animal
     animals[i].update();
+
   }
 }
 
-// updateSausageDog()
 // Calls the update() method of the sausage dog
 function updateSausageDog() {
+
   sausageDog.update();
+
 }
 
-// mousePressed()
-// Automatically called by p5 when the mouse is pressed.
-// Call the sausage dog's mousePressed() method so it knows
-// the mouse was clicked.
+// Handle mouse clicks
 function mousePressed() {
+
+  // If the mouse is pressed, check if it was on the sausage dog
   sausageDog.mousePressed();
+
 }
