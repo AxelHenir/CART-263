260e6c615654443d06c3fbffb6a7e26f6ae1b12e
AxelHenir <axelhenirmail@gmail.com>
2022-02-15 17:44:05 -0500

    P1: Started!

---
+++ Projects/P1 - Enders Game/js/sketch.js
@@ -0,0 +1,15 @@
+// CART 263 Project 1 - Ender's Game
+// Alex Henri - Concordia University Winter 2022
+
+
+function preload(){
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
---
+++ Projects/P1 - Enders Game/sketch.js
@@ -0,0 +1,15 @@
+// Project 1 - Ender's Game 
+// Alex Henri - Concodia University 2022
+
+
+function preload(){
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
