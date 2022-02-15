0d69d4d200203ed46d644d7b008f833e11071f6a
AxelHenir <axelhenirmail@gmail.com>
2022-02-15 18:09:28 -0500

    P1: Added new state machine and basic controls

--- Projects/P1 - Enders Game/js/sketch.js
+++ Projects/P1 - Enders Game/js/sketch.js
@@ -1,6 +1,6 @@
 // CART 263 Project 1 - Ender's Game
 // Alex Henri - Concordia University Winter 2022
-
+let script = undefined;
 
 function preload(){
 
@@ -8,8 +8,26 @@ function preload(){
 
 function setup() {
 
+  script = new State();
+
 }
 
 function draw() {
+
+}
+
+// Handles keyboard input
+function keypressed(){
+
+  switch(keyCode){
+
+    case 81:
+    break;
+
+  }
+}
+
+// Playhead will display the contents located at Act A, Scene B, Line C.
+function playhead(){
 
 }
