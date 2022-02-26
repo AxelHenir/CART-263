e4163305ded831a10fb9edaad895144bddc49af6
AxelHenir <axelhenirmail@gmail.com>
2022-02-26 11:08:33 -0500

    P1: Added local storage support
    + Added a save and load function for local storage
    + Save the scene with (S) and load it with (W)

---
+++ Projects/P1 - Enders Game/js/README.md
@@ -0,0 +1,5 @@
+# CART 263 Project 1 - Night at the Movies
+
+### Alex Henri, 40108348, Project 1: Ender's Game
+
+Artist's Statement:
--- Projects/P1 - Enders Game/js/sketch.js
+++ Projects/P1 - Enders Game/js/sketch.js
@@ -4,7 +4,7 @@
 // State machine to track program state
 let state = undefined;
 
-// Script for the game
+// Script for the game, (literally the script)
 let playbook = undefined;
 const SCRIPT_LOCATION = "assets/json/script.json";
 
@@ -13,6 +13,9 @@ let gpu = undefined;
 
 // An audio driver for all of our sounds
 let audio = undefined;
+
+// A name to save our data to
+const SAVED_DATA = "enders-game-data";
 
 function preload(){
 
@@ -39,10 +42,47 @@ function setup() {
 
 }
 
+// Driver function of the program - calls graphics to display the scene
 function draw() {
 
+  // Graphics object will draw our scene
   gpu.displayScene(state.scene);
   
+}
+
+// Runs at start of project, checks for saved data in the browser and loads it if found
+function loadSavedData(){
+
+  let d = JSON.parse(localStorage.getItem(SAVED_DATA));
+
+        // Check if there was data to load
+        if (d) {
+          
+          state.sceneCounter = d.sceneCounter - 1;
+
+        }
+
+        state.nextScene();
+
+        console.log("LOADED SCENE: ",state.sceneCounter);
+}
+
+// Saves the current state of the program in browser storage 
+function saveState(){
+
+  // Create the object that holds the data we want to save
+  let saveData = {
+
+        // Scene counter
+        sceneCounter: state.sceneCounter,
+
+  }
+
+  // Save the data to browser memory 
+  localStorage.setItem(SAVED_DATA, JSON.stringify(saveData));
+
+  console.log(`Saved ${JSON.parse(localStorage.getItem(SAVED_DATA)).sceneCounter} to local storage`);
+
 }
 
 // Handles keyboard input
@@ -50,9 +90,32 @@ function keyPressed(){
 
   switch(keyCode){
 
-    case 81: // Q = next line
+    case 81: // Q = Next Line
 
+      // Next scene
       gpu.next();
+
+      break;
+
+    case 83: // S = Save data
+
+      // Saves the current state to browser memory
+      saveState();
+
+      break;
+
+    case 88: // X = Clear Browser Memory
+
+      // Clears browser memory (DEBUG COMMAND)
+      localStorage.clear();
+      console.log("SAVE DELETED.");
+
+      break;
+
+    case 87: // W = Load saved data (DEBUG COMMAND)
+
+      // Loads the saved data in browser storage
+      loadSavedData();
 
       break;
 
