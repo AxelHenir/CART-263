0c2f6cf4c14c8a1cf8ffd9240f7693cee09ad5be
AxelHenir <axelhenirmail@gmail.com>
2022-04-13 03:13:42 -0400

    P2; Great Grid Gro

--- Projects/P2 - Generative_Art/js/script.js
+++ Projects/P2 - Generative_Art/js/script.js
@@ -1,10 +1,11 @@
 
-let cols = 3;
+let cols = 2;
 let rows = 3;
 
 let diagram;
 
-const CHANCE_HAS_CHILD = 0.5;
+const CHANCE_HAS_CHILD = 0.65;
+const MIN_WIDTH = 15;
 
 function preload(){
 
@@ -14,16 +15,25 @@ function setup(){
 
     createCanvas(1000,1000);
 
+    diagram = new Cell(cols,rows,width,height);
+    diagram.newCell();
+
 }
 
 function draw(){
 
 }
 
-function Cell(x,y,rows,cols,width,height){
+function keyPressed(){
+    switch(keyCode){
+        case 81:
+            diagram = new Cell(rows,cols,width,height);
+            diagram.newCell();
+            break;
+    }
+}
 
-    this.x = x;
-    this.y = y;
+function Cell(rows,cols,width,height){
 
     this.r = rows;
     this.c = cols;
@@ -35,31 +45,47 @@ function Cell(x,y,rows,cols,width,height){
 
     this.newCell = function(){
 
-        push();
-        rectMode(CENTER,CENTER);
-        fill(this.fill);
-        noStroke();
-        rect(0,0,this.w,this.h);
+        console.log(this.w)
 
-        for(let i = 0 ; i < rows ; i ++){
-            for(let j = 0 ; j < cols ; j ++){
+        if(this.w > MIN_WIDTH){
 
-                let l = i*(width/rows) + 0.5(width/rows);
-                let k = j*(height/cols) + 0.5(height/cols);
-
-                // l,k = center x,y of ith,jth cell
-
-                
-
-                
-                let newCell = new Cell();
-                newCell.newCell();
-
+            push();
+            rectMode(CORNER);
+            fill(this.fill);
+            noStroke();
+            rect(0,0,this.w,this.h);
+            pop();
+    
+            for(let i = 0 ; i < this.r ; i ++){
+                for(let j = 0 ; j < this.c ; j ++){
+    
+                    let l = i*(this.h/this.r);
+                    let k = j*(this.w/this.c);
+    
+                    push();
+                    //console.log(l,k);
+                    translate(l,k);
+    
+                    let r = random();
+    
+                    if(r < CHANCE_HAS_CHILD){
+    
+                        let newCell = new Cell(this.r,this.c,this.h/this.r,this.w/this.c);
+                        newCell.newCell();
+    
+                    } else {
+    
+                    }
+    
+                    pop();
+    
+                }
             }
+
+        } else {
+
         }
-
-
-        pop();
+        
 
     }
 
