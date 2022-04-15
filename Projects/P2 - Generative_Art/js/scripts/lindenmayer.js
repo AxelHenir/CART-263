ec302ec506ee4f9ba1f55356ee5b3df25caaf261
AxelHenir <axelhenirmail@gmail.com>
2022-04-15 18:57:12 -0400

    P2: Bugfix

--- Projects/P2 - Generative_Art/js/scripts/lindenmayer.js
+++ Projects/P2 - Generative_Art/js/scripts/lindenmayer.js
@@ -1,20 +1,37 @@
 let c;
 
-let axiom = "F";
-
-let sentence = axiom;
-let len = 50;
+let sentence = "F";
+let len = 100;
+let angle;
 
 let rules = [];
 
 rules.push({a: "F", b: "FF+[+F-F-F]-[-F+F+F]"});
 
-function interpret(){
+function setup(){
+
+    angle = radians(25);
+
+    c = createCanvas(1000,1000);
+    c.parent("canvasContainer");
+
+    drawSentence();
+
+}
+
+// Reads the sentence and draws it according to a set of instructions
+function drawSentence(){
+
+    background(255);
 
     resetMatrix();
     translate(width/2, height);
 
+    //console.log(sentence);
+
+    // For each character in the sentence, do its associated instruction
     for (let i = 0; i< sentence.length; i++){
+
         let current = sentence.charAt(i);
 
         switch(current){
@@ -25,11 +42,11 @@ function interpret(){
                 break;
 
             case "+":
-                rotate(PI/6);
+                rotate(angle);
                 break;
 
             case "-":
-                rotate(-PI/6);
+                rotate(-angle);
                 break;
 
             case "[":
@@ -45,41 +62,57 @@ function interpret(){
     }
 }
 
-function generate(){
-   //len *= 0.5;
+// Analyzes current sentence and generates a new one based on the rules, calls to draw the sentence afterwards
+function generateSentence(){
+
+    // Decrease length of branches
+    len *= 0.60;
+
+    // Empty temporary sentence
     let nextSentence = "";
+
+    // For each character in the current sentence, check it against all rules
     for (let i = 0; i < sentence.length ; i ++){
+
+        let current = sentence.charAt(i);
+        let foundCharWithRule = false;
 
         for (let j = 0 ; j < rules.length; j++){
 
-            let current = sentence.charAt(i);
-
+            // If a character matches a rule, add the result of the rule to the temp
             if(current == rules[j].a){
                 nextSentence += rules[j].b;
-                foundRule = true;
+                foundCharWithRule = true;
                 break;
             }
 
         }
 
+        if(!foundCharWithRule){
+
+            // Otherwise, if no rule is associated with the character, simply add the character to temp
+            nextSentence += current;
+        }
+
     }
 
+    // Set the sentence to the temp
     sentence = nextSentence;
-    interpret();
-}
 
-
-function setup(){
-
-    c = createCanvas(1000,1000);
-    c.parent("canvasContainer");
-
+    // Illustrate the sentence
+    drawSentence();
 }
 
 function keyPressed(){
     switch(keyCode){
         case 81:
             generate();
+            break;
+
+        case 82:
+            sentence="F";
+            len=100;
+            drawSentence();
             break;
 
         case 83:
