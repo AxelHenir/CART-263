65637eab0956be29eafd4f08d9bb0b5c5c5f22d7
AxelHenir <axelhenirmail@gmail.com>
2022-04-18 16:49:44 -0400

    P2: added more systems to lindenmayer

--- Projects/P2 - Generative_Art/js/scripts/lindenmayer.js
+++ Projects/P2 - Generative_Art/js/scripts/lindenmayer.js
@@ -1,22 +1,52 @@
 let c;
 
-let sentence = "F";
-let len = 100;
-let angle;
+let sentence;
+let len, angle, startingPt;
 
 let rules = [];
 
-rules.push({a: "F", b: "FF+[+F-F-F]-[-F+F+F]"});
+let RULESETS;
 
 function setup(){
-
-    angle = radians(25);
 
     c = createCanvas(1000,1000);
     c.parent("canvasContainer");
 
+    RULESETS = [
+        [{axiom: "F", angle: 25, start: {x: width/2, y: height}},{a: "F", b: "FF+[+F-F-F]-[-F+F+F]"}], // Tree1 (25 deg)
+        [{axiom: "G", angle: 25, start: {x: width/2, y: height}},{a: "G", b: "F+[[G]-G]-F[-FG]+G"}, {a: "F", b:"FF"}], // Tree2 (25 deg)
+        [{axiom: "F", angle: 90, start: {x: width/2, y: height/2}},{a: "F", b: "F+G"}, {a: "G", b:"F-G"}], // Dragon curve (90 deg)
+        [{axiom: "F", angle: 60, start: {x: width/2, y: height}},{a: "F", b: "G-F-G"}, {a: "G", b: "F+G+F"}], // Sierpinsky hexagon (60 deg)
+        [{axiom: "F-G-G", angle: 120, start: {x: width, y: height}},{a: "F", b: "F-G+F+G-F"}, {a: "G", b: "GG"}], // Sierpinsky triangle (120 deg)
+        [{axiom: "A", angle: 90, start: {x: 0, y: height}},{a: "A", b: "+BF-AFA-FB+"}, {a: "B", b: "-AF+BFB+FA-"}] // Hilbert Curve (90 deg)
+    ];
+
+    newDiagram();
+
+}
+
+// Generates a new diagram by selecting new rules and resetting all parameters
+function newDiagram(){
+
+    selectRules();
+
+    len = document.getElementById("length").value;
+
     drawSentence();
 
+}
+
+function selectRules(){
+    rules = [];
+    let temp = random(RULESETS);
+    temp.forEach(rule => rules.push(rule));
+    
+    let config = rules[0];
+    rules.splice(0,1);
+
+    sentence = config.axiom;
+    angle = config.angle;
+    startingPt = config.start;
 }
 
 // Reads the sentence and draws it according to a set of instructions
@@ -25,7 +55,7 @@ function drawSentence(){
     background(255);
 
     resetMatrix();
-    translate(width/2, height);
+    translate(startingPt.x, startingPt.y);
 
     // For each character in the sentence, do its associated instruction
     for (let i = 0; i< sentence.length; i++){
@@ -39,11 +69,18 @@ function drawSentence(){
                 translate(0,-len);
                 break;
 
+            case "G":
+                line(0,0,0,-len);
+                translate(0,-len);
+                break;
+
             case "+":
+                angleMode(DEGREES);
                 rotate(angle);
                 break;
 
             case "-":
+                angleMode(DEGREES);
                 rotate(-angle);
                 break;
 
@@ -63,8 +100,10 @@ function drawSentence(){
 // Analyzes current sentence and generates a new one based on the rules, calls to draw the sentence afterwards
 function generateSentence(){
 
+    //angle = document.getElementById("angle").value;
+
     // Decrease length of branches
-    len *= 0.60;
+    len *= document.getElementById("truncation").value;
 
     // Empty temporary sentence
     let nextSentence = "";
@@ -76,26 +115,23 @@ function generateSentence(){
         let foundCharWithRule = false;
 
         for (let j = 0 ; j < rules.length; j++){
-
             // If a character matches a rule, add the result of the rule to the temp
             if(current == rules[j].a){
                 nextSentence += rules[j].b;
                 foundCharWithRule = true;
                 break;
             }
-
         }
 
         if(!foundCharWithRule){
-
             // Otherwise, if no rule is associated with the character, simply add the character to temp
             nextSentence += current;
         }
-
     }
 
     // Set the sentence to the temp
     sentence = nextSentence;
+    console.log(sentence);
 
     // Illustrate the sentence
     drawSentence();
@@ -103,17 +139,15 @@ function generateSentence(){
 
 function keyPressed(){
     switch(keyCode){
-        case 81:
+        case 81: // Q - grow
             generateSentence();
             break;
 
-        case 82:
-            sentence="F";
-            len=100;
-            drawSentence();
+        case 82: // R - restart 
+            newDiagram();
             break;
 
-        case 83:
+        case 83: // S - Save
             saveCanvas("Grid","png");
             break;
     }
