6abc70f2f1b3223019a05814ce72ef3086decfba
AxelHenir <axelhenirmail@gmail.com>
2022-04-18 23:52:05 -0400

    P2: All pages done

--- Projects/P2 - Generative_Art/js/scripts/voronoi.js
+++ Projects/P2 - Generative_Art/js/scripts/voronoi.js
@@ -1,33 +1,96 @@
 let c;
 
-const NUM_CELLS = 200;
-const JITTER = false; 
-
 let cells = [];
+let palette;
+
+const PALETTES = [
+
+    // Pastel
+    ["#FBF8CC","#FDE4CF","#FFCFD2","#F1C0E8","#CFBAF0","#A3C4F3","#90DBF4","#8EECF5","#98F5E1","#B9FBC0",],
+    // Synthwave
+    ["#FFBE0B","#FB5607","#FF006E","#8338EC","#3A86FF",],
+    // Coffee
+    ["#EDE0D4","#E6CCB2","#DDB892","#B08968","#7F5539",],
+    // Red n Blue
+    ["#e63946", "#f1faee", "#a8dadc", "#457b9d", "#1d3557"],
+    // Forest
+    ["#e9f5db", "#cfe1b9", "#b5c99a", "#97a97c", "#87986a", "#718355"],
+    // Heatwave
+    ["#ff7b00", "#ff8800", "#ff9500", "#ffa200", "#ffaa00", "#ffb700", "#ffc300", "#ffd000", "#ffdd00", "#ffea00"],
+    // Reptile
+    ["#05668d","#028090","#00a896","#02c39a","#f0f3bd"],
+    // Purp
+    ["#240046","#3c096c","#5a189a","#7b2cbf","#9d4edd","#c77dff","#e0aaff"],
+
+];
+
+document.getElementById("generateButton").addEventListener("click",generateVoronoiDiagram);
+document.getElementById("newButton").addEventListener("click",newDiagram);
+document.getElementById("saveButton").addEventListener("click",saveDiagram);
 
 function setup(){
 
     c = createCanvas(1000,1000);
     c.parent("canvasContainer");
 
-    generateVoronoiDiagram();
+    newDiagram();
 
 }
 
 function generateVoronoiDiagram(){
 
-    background(60);
+    background(random(palette));
 
-    for ( let i = 0 ; i < NUM_CELLS ; i ++ ){
-    
-        voronoiSite(random(width), random(height), random(255));
-    
+    let numCells = int(document.getElementById("numCells").value);
+
+    // Randomly generate all the sites
+    for ( let i = 0 ; i < numCells ; i ++ ){
+        voronoiSite(random(width), random(height), random(palette));
+    }
+
+    // Edge stroke
+    if(int(document.getElementById("strokeWeight").value) > 0){
+        voronoiCellStroke(255);
+        voronoiCellStrokeWeight(int(document.getElementById("strokeWeight").value));
+    } else {
+        voronoiCellStroke(255);
+        voronoiCellStrokeWeight(0);
+    }
+
+    // Site Stroke 
+    if(int(document.getElementById("siteStrokeWeight").value) > 0){
+        voronoiSiteStroke(255);
+        voronoiSiteStrokeWeight(int(document.getElementById("siteStrokeWeight").value));
+    } else {
+        voronoiSiteStroke(255);
+        voronoiSiteStrokeWeight(0);
+    }
+
+    // Jitter amount
+    if(int(document.getElementById("jitterAmount").value) > 0){
+
+        voronoiJitterStepMax(30);
+        voronoiJitterStepMin(10);
+        voronoiJitterFactor(document.getElementById("jitterAmount").value);
+
+        voronoi(1000,1000,true);
+    } else {
+        voronoi(1000,1000,false);
     }
     
-    voronoi(1000,1000,JITTER);
-
+    // Draw the diagram
     voronoiDraw(0,0,1000,1000);
 
+}
+
+function newDiagram(){
+    palette = random(PALETTES);
+    voronoiClearSites();
+    generateVoronoiDiagram();
+}
+
+function saveDiagram(){
+    saveCanvas("Voronoi","png");
 }
 
 function keyPressed(){
@@ -37,12 +100,11 @@ function keyPressed(){
             break;
 
         case 82:
-            voronoiClearSites();
-            generateVoronoiDiagram();
+            newDiagram();
             break;
 
         case 83:
-            saveCanvas("Voronoi","png");
+            saveDiagram();
             break;
     }
 }
